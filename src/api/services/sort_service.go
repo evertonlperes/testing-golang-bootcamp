162616fabcf92f40599493b90187e8e828fbd178
Everton <evertonperes@Evertons-MacBook-Pro.local>
2022-03-10 14:36:26 -0600

    Adding a few tests

--- src/api/services/sort_service.go
+++ src/api/services/sort_service.go
@@ -2,6 +2,11 @@ package services
 
 import "testing-golang/src/api/utils/sort"
 
+const (
+	privateConst = "private"
+	publicConst  = "public"
+)
+
 func Sort(elements []int) {
 	sort.BubbleSort(elements)
 }
---
+++ src/api/services/sort_service_test.go
@@ -0,0 +1,28 @@
+package services
+
+import "testing"
+
+func TestConstant(t *testing.T) {
+	if privateConst != "private" {
+		t.Error("The privateConst should be 'private'. Found", privateConst)
+	}
+
+	if publicConst != "public" {
+		t.Error("The publicConst should be 'public'. Found", privateConst)
+	}
+
+}
+
+func TestSort(t *testing.T) {
+	elements := []int{9, 6, 5, 3, 4, 7, 1, 2, 3, 0}
+
+	Sort(elements)
+
+	if elements[0] != 9 {
+		t.Error("First element should be 9. Found", elements[0])
+	}
+
+	if elements[len(elements)-1] != 0 {
+		t.Error("Last element should be 0. Found", elements[len(elements)-1])
+	}
+}
