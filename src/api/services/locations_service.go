57a3aeeb44ab5ba1812f8fca61cf7d50f9e06662
Everton <evertonperes@Evertons-MacBook-Pro.local>
2022-03-15 10:03:06 -0600

    Added provider tests: integration and unit tests

--- src/api/services/locations_service.go
+++ src/api/services/locations_service.go
@@ -6,6 +6,20 @@ import (
 	"testing-golang/src/providers/locations_provider"
 )
 
-func GetCountry(countryId string) (*locations.Country, *errors.ApiError) {
+type locationsService struct{}
+
+type locationsServiceInterface interface {
+	GetCountry(countryId string) (*locations.Country, *errors.ApiError)
+}
+
+var (
+	LocationsService locationsServiceInterface
+)
+
+func init() {
+	LocationsService = &locationsService{}
+}
+
+func (s *locationsService) GetCountry(countryId string) (*locations.Country, *errors.ApiError) {
 	return locations_provider.GetCountry(countryId)
 }
